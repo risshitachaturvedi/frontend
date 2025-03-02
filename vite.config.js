8ce3739462ca4df3957a2a60c959a3638d2c36e4
risshitachaturvedi <rishitachaturvedi@gmail.com>
2025-03-02 11:03:14 +0530

    removed ViteStaticCopy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { viteStaticCopy } from "vite-plugin-static";
+//import { ViteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteStaticCopy()],
-  base: "/mini-map/",
+  plugins: [
+    react(),
+    tailwindcss(),
+    // ViteStaticCopy({
+    //   targets: [
+    //     {
+    //       src: "frontend/public",
+    //       dest: "frontend/dist",
+    //     },
+    //   ],
+    // }),
+  ],
+  //base: "/mini-map/",
   server: {
     host: "0.0.0.0", // Expose to local network
     port: 5173, // Change if needed
