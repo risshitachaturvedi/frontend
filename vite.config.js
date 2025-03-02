0f35b6eeaedae64d013d349da7831f0fa16c744a
risshitachaturvedi <rishitachaturvedi@gmail.com>
2025-03-02 18:02:57 +0530

    hh

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-//import { ViteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    // ViteStaticCopy({
-    //   targets: [
-    //     {
-    //       src: "frontend/public",
-    //       dest: "frontend/dist",
-    //     },
-    //   ],
-    // }),
-  ],
-  //base: "/mini-map/",
+  plugins: [react(), tailwindcss()],
+  base: "/mini-map/", // Put both plugins inside the array
   server: {
     host: "0.0.0.0", // Expose to local network
     port: 5173, // Change if needed
